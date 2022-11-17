8072d8bd833c01f541f71484c1a95f98a65be6f1
dev-madilyn <lollin0@naver.com>
2022-11-17 14:21:11 +0900

    Chore: 공연 전체 조회 index #6

--- CDS-Server/src/index.ts
+++ CDS-Server/src/index.ts
@@ -1,22 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
-import router from "./router";
+import showRouter from "./router/showRouter";
 
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
-app.use("/api", router); // use -> 모든 요청
-// localhost:8000/api -> api 폴더
-// localhost:8000/api/user -> user.ts
-
 //* HTTP method - GET
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("마! 이게 서버다!!!!!!!!!!!!!!!!!!!!");
+    res.send("마! 이게 서버다!!!!!!!!!!!!!!!!!!!!");
 });
 
+//* 공연 전체 조회
+app.get("/show", showRouter);
+
 app.listen(PORT, () => {
-  console.log(`
+    console.log(`
         #############################################
             🛡️ Server listening on port: ${PORT} 🛡️
         #############################################
