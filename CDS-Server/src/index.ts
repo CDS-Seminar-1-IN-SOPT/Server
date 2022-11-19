52d450ad7c429a0c58c72c6e73205e74bb8fa9db
dev-madilyn <lollin0@naver.com>
2022-11-19 13:26:43 +0900

    Chore: cors 적용

--- CDS-Server/src/index.ts
+++ CDS-Server/src/index.ts
@@ -2,10 +2,12 @@ import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
 import showRouter from "./router/showRouter";
 import scheduleRouter from "./router/scheduleRouter";
+import cors from 'cors';
 
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
 
+app.use(cors());
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
 app.use("/", router); // use -> 모든 요청
@@ -13,10 +15,6 @@ app.use("/", router); // use -> 모든 요청
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("마! 이게 서버다!!!!!!!!!!!!!!!!!!!!");
 });
-
-app.get("/show", showRouter);
-app.get("/show/:showId", showRouter);
-app.get("/schedule/:showId", scheduleRouter);
 
 app.listen(PORT, () => {
     console.log(`
